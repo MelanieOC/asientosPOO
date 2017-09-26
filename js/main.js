71763b6e494fdcb16977898c3d9605c80a4c9000
MelanieOC <melanie.ocharan@gmail.com>
2017-09-26 06:39:08 -0500

    corrigiendo pequeños errores

--- js/main.js
+++ js/main.js
@@ -8,108 +8,121 @@ class Pasajero {
 
     toHTML() {
         let html = `<div class="well lista">
-                    <strong>Asiento N°${this.numero}</strong><br>
-                    <strong>Pasajero: </strong>${this.nombre} ${this.apellido}<br>
-                    <strong>DNI: </strong>${this.dni}<br>
-                 </div>`
+                        <strong>Asiento N°${this.numero}</strong><br>
+                        <strong>Pasajero: </strong>${this.nombre} ${this.apellido}<br>
+                        <strong>DNI: </strong>${this.dni}<br>
+                    </div>`
         return html;
     }
 }
 
-class Bus{
-    constructor(){
-        this.asientos=[];
-        this.numeroAsientos=undefined;
-        this.numAsiento=undefined;
-        this.asientoSeleccionado=undefined;
+class Bus {
+    constructor() {
+        this.asientos = [];
+        this.numeroAsientos = undefined;
+        this.numAsiento = undefined;
+        this.asientoSeleccionado = undefined;
     }
-    dibujarTabla(){
-        let tabla=$('<div>')
+    dibujarTabla() {
+        let tabla = $('<div>')
         for (let j = 0; j < this.numeroAsientos; j += 4) {
             let fila = $('<div>').addClass('fila');
-          for (let i = j + 1; i <= j + 4; i++) {
-            let asiento = this.asientos[i - 1];
-            let celda = $('<button>').addClass('celda desocupado').html(i);
-            if (i==j+2) {
-                celda.css('margin-right','45px');
+            for (let i = j + 1; i <= j + 4; i++) {
+                let asiento = this.asientos[i - 1];
+                let celda = $('<button>').addClass('celda desocupado').html(i);
+                if (i == j + 2) {
+                    celda.css('margin-right', '45px');
+                }
+                celda.appendTo(fila).click((e) => this.elegirAsiento(e.currentTarget));
             }
-            celda.appendTo(fila).click((e)=>this.elegirAsiento(e.currentTarget));
-          }
-          tabla.append(fila);
+            tabla.append(fila);
         }
         return tabla;
     }
-    formulario(){
-        $('#nombre').prop('readonly',false);
-        $('#apellido').prop('readonly',false);
-        $('#dni').prop('readonly',false);
-        $('#nombre').focus();
-        $('#btnReservar').click(()=>this.reservar());
-    }
-    elegirAsiento(div){
-        $('.desocupado').css('background-color','#57EC89');
-        this.numAsiento=parseInt(div.textContent);
-        this.asientoSeleccionado=$(div);
-        if(this.asientos[this.numAsiento-1]==undefined){
-            this.asientoSeleccionado.css('background-color','orange');
-            this.formulario();
+    elegirAsiento(div) {
+        $('.desocupado').css('background-color', '#57EC89');
+        this.numAsiento = parseInt(div.textContent);
+        this.asientoSeleccionado = $(div);
+        if (this.asientos[this.numAsiento - 1] == undefined) {
+            $("#numero").html(`Asiento Nº ${this.numAsiento}`);
+            this.asientoSeleccionado.css('background-color', 'orange');
+            $('#nombre').focus();
+            $('#btnReservar').prop('disabled', false);
         } else {
-            this.cancelar();
+            this.liberar();
         }
     }
-    reservar(){
+    reservar() {
         let nombre = $('#nombre').val();
-        let apellido= $('#apellido').val();
-        let dni=$('#dni').val();
+        let apellido = $('#apellido').val();
+        let dni = $('#dni').val();
         if (nombre != '' && apellido != '' && dni != '') {
-            this.asientos[this.numAsiento-1]= new Pasajero(this.numAsiento, nombre, apellido, dni); 
-            this.asientoSeleccionado.removeClass('desocupado').addClass('ocupado').css('background-color','red').prop('disabled',true);
+            this.asientos[this.numAsiento - 1] = new Pasajero(this.numAsiento, nombre, apellido, dni);
+            this.asientoSeleccionado.removeClass('desocupado').addClass('ocupado').css('background-color', 'red').prop('disabled', true);
             this.reiniciar();
         } else {
             console.log('faltan datos');
         }
     }
-    liberar(){
-        $("#liberar .well").remove()
-        $('.desocupado').css('background-color','#57EC89').prop('disabled',true);
-        $(".ocupado").prop('disabled',false);
+    tabLiberar() {
+        $("#liberar").html('<p>Escoja el asiento reservado a liberar</p>')
+        $('.desocupado').css('background-color', '#57EC89').prop('disabled', true);
+        $(".ocupado").prop('disabled', false);
     }
-    cancelar(){
-        let mostrar=this.asientos[this.numAsiento-1].toHTML();
-        $(mostrar).append('<button class="btn">Cancelar</button>');
+    liberar() {
+        $("#liberar").empty();
+        let mostrar = this.asientos[this.numAsiento - 1].toHTML();
         $("#liberar").append(mostrar);
+        $('<button>').addClass('btn').html('Liberar').click(() => this.cancelar()).appendTo("#liberar");
     }
-    listar(){
-        let ocupados = this.asientos.filter(a=>!undefined);
-        if(ocupados.length==0){
+    cancelar() {
+        this.asientos[this.numAsiento - 1] = undefined;
+        this.asientoSeleccionado.removeClass('ocupado').addClass('desocupado').css('background-color', '#57EC89').prop('disabled', true);
+    }
+    listar() {
+        let ocupados = this.asientos.filter(a => !undefined);
+        if (ocupados.length == 0) {
             $("#listar").html('<div class="alert alert-warning" role="alert">Aún no hay pasajeros</div>');
         } else {
-            let lista='';
-            ocupados.forEach(a=>lista+=a.toHTML());
+            let lista = '';
+            ocupados.forEach(a => lista += a.toHTML());
             $("#listar").html(lista);
         }
     }
-    reiniciar(){
-        $(".desocupado").prop('disabled',false);
-        $(".ocupado").prop('disabled',true);
-        $('#nombre').val('').prop('readonly',true);
-        $('#apellido').val('').prop('readonly',true);
-        $('#dni').val('').prop('readonly',true);
-        $('#btnReservar').off('click');
+    buscar(){
+        $("#encontrados").empty();
+        let busqueda = $('#busqueda').val();
+        let encontrado = this.asientos.filter(a=>!undefined&&a.dni==busqueda);
+        if (encontrado.length == 0) {
+            $("#encontrados").html('<div class="alert alert-warning" role="alert">Sin resultados</div>');
+        } else {
+            let lista = '';
+            encontrado.forEach(a => lista += a.toHTML());
+            $("#encontrados").html(lista);
+        }
+        $('#busqueda').val('');
     }
-    eventos(){
-        $("#tabListar").click(()=>this.listar());
-        $("#tabLiberar").click(()=>this.liberar());
-        $("#tabReservar").click(()=>this.reiniciar());
+    reiniciar() {
+        $(".desocupado").prop('disabled', false);
+        $(".ocupado").prop('disabled', true);
+        $('#btnReservar').prop('disabled', true);
     }
-    iniciar(){
-        this.numeroAsientos=32;
+    eventos() {
+        $("#tabListar").click(() => this.listar());
+        $("#tabLiberar").click(() => this.tabLiberar());
+        $("#tabReservar").click(() => this.reiniciar());
+        $('#btnReservar').click(() => this.reservar());
+        $('#btnBuscar').click(() => this.buscar());
+        $('#tabBuscar').click(() => $("#encontrados").empty());
+    }
+    iniciar() {
+        this.numeroAsientos = 32;
         $('#asientos').html(this.dibujarTabla());
         this.eventos();
     }
 }
 
-$(document).ready(()=>{
-    let asientos=new Bus();
+$(document).ready(() => {
+    let asientos = new Bus();
     asientos.iniciar();
 })
